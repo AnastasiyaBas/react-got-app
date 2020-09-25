50ba9ff4e3bd50d8e901da6b128e3ae6949c47b3
Anastasiya Bas <nastya.mejerow@gmail.com>
2020-09-25 10:59:11 +0200

    refactoring code

---
+++ src/components/pages/index.js
@@ -0,0 +1,10 @@
+import CharacterPage from './charaterPage';
+import BooksPage from './booksPage';
+import HousesPage from './housesPage';
+import BooksItem from './booksItem';
+export {
+    CharacterPage,
+    BooksPage,
+    HousesPage,
+    BooksItem
+}
